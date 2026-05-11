ff97a37cda729c4af700886b8208488bf0b2046e
abhay0826R-dev <abhay0826r@gmail.com>
2026-05-11 09:12:31 +0530

    header: added dropdown-menus

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,14 @@ const App = () => {
   return (
     <>
       <header>
-        <h1 className='main-head'>Job Portal</h1>
+        <div>
+          <h1 className='main-head'>Job Portal</h1>
+        </div>
+        <div className='dropdown-menus'>
+          <span>Browse Jobs ▾</span>
+          <span>Saved Jobs ▾</span>
+          <span>Profile ▾</span>
+        </div>
       </header>
       <JobCards />
       <footer>
